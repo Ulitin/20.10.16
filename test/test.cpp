348141dfb0f7542c2f4e2a85bd72337660dfaaa4
Ulitin <sashavladimirovichulitin@gmail.com>
2016-11-04 00:35:10 +0300

    solved

--- test/test.cpp
+++ test/test.cpp
@@ -1,94 +1,63 @@
 #include <gtest/gtest.h>
-#include"polinbrom.h"
+#include <stack.h>
 
-TEST(Addition, correct_with_zero)
-{
-	EXPECT_TRUE(polindrom_or_not(0));
+TEST(Addition, correct_with_zero) {
+    stack a;
+    ASSERT_ANY_THROW(a.min());
 }
 
-TEST(Addition, correct_with_one_element)
-{
-	nambers* first;
-	first=new nambers;
-	first->date=5;
-	first->next=0;
-	EXPECT_TRUE(polindrom_or_not(first));
+TEST(Addition, found_min_in_two_el_1) {
+    stack a;
+    a.push(25);
+    a.push(0);
+    EXPECT_EQ(0 , a.min());
 }
 
-TEST(Addition, correct_with_five_element)
-{
-	nambers* first, *box;
-	first=new nambers;
-	box=first;
-	for(int i=0; i<5; i++)
-	{
-		box->date=5;
-		if(i!=4)
-		{
-			box->next=new nambers;
-			box=box->next;
-		}
-		box->next=0;
-	}
-	EXPECT_TRUE(polindrom_or_not(first));
+TEST(Addition, found_min_in_two_el_2) {
+    stack a;
+    a.push(25);
+    a.push(150);
+    EXPECT_EQ(25 , a.min());
 }
 
-TEST(Addition, ancorrect_with_five_element)
-{
-	nambers* first, *box;
-	first=new nambers;
-	box=first;
-	for(int i=0; i<5; i++)
-	{
-		box->date=i;
-		if(i!=4)
-		{
-			box->next=new nambers;
-			box=box->next;
-		}
-		box->next=0;
-	}
-	EXPECT_FALSE(polindrom_or_not(first));
+TEST(Addition, found_min_in_three_el_1) {
+    stack a;
+    a.push(25);
+    a.push(150);
+    a.push(-10);
+    EXPECT_EQ(-10 , a.min());
 }
 
-TEST(Addition, correct_with_four_element)
-{
-	nambers* first, *box;
-	first=new nambers;
-	box=first;
-	for(int i=0; i<4; i++)
-	{
-		box->date=1;
-		if(i!=3)
-		{
-			box->next=new nambers;
-			box=box->next;
-		}
-		box->next=0;
-	}
-	EXPECT_TRUE(polindrom_or_not(first));
+TEST(Addition, found_min_in_three_el_2) {
+    stack a;
+    a.push(25);
+    a.push(-25);
+    a.push(-10);
+    EXPECT_EQ(-25 , a.min());
 }
 
-TEST(Addition, ancorrect_with_four_element)
-{
-	nambers* first, *box;
-	first=new nambers;
-	box=first;
-	for(int i=0; i<4; i++)
-	{
-		box->date=i;
-		if(i!=3)
-		{
-			box->next=new nambers;
-			box=box->next;
-		}
-		box->next=0;
-	}
-	EXPECT_FALSE(polindrom_or_not(first));
+TEST(Addition, found_min_in_three_el_3) {
+    stack a;
+    a.push(-15);
+    a.push(150);
+    a.push(-10);
+    EXPECT_EQ(-15 , a.min());
 }
 
-int main(int ac, char* av[])
-{
+TEST(Addition, test_on_top_and_pop) {
+    stack a;
+    a.push(-15);
+    a.push(150);
+    EXPECT_EQ(150 , a.top());
+    a.pop();
+    EXPECT_EQ(-15 , a.top());
+    a.pop();
+    EXPECT_FALSE(a.pop());
+    ASSERT_ANY_THROW(a.top());
+}
+
+int main(int ac, char* av[]) {
   testing::InitGoogleTest(&ac, av);
   return RUN_ALL_TESTS();
 }
+
