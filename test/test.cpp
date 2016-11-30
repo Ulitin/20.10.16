95294f02eeb3fd9c7cd8ffca47a9ef15d7c69220
Ulitin <sashavladimirovichulitin@gmail.com>
2016-11-30 23:06:39 +0300

    Merged master fixed conflict

--- test/test.cpp
+++ test/test.cpp
@@ -1,48 +1,7 @@
 #include <gtest/gtest.h>
-#include "stack.h"
 
-TEST(Addition, correct_with_zero) {
-    stack a;
-    ASSERT_ANY_THROW(a.min());
-}
 
-TEST(Addition, found_min_in_two_el_1) {
-    stack a;
-    a.push(25);
-    a.push(0);
-    EXPECT_EQ(0 , a.min());
-}
 
-TEST(Addition, found_min_in_two_el_2) {
-    stack a;
-    a.push(25);
-    a.push(150);
-    EXPECT_EQ(25 , a.min());
-}
-
-TEST(Addition, found_min_in_three_el_1) {
-    stack a;
-    a.push(25);
-    a.push(150);
-    a.push(-10);
-    EXPECT_EQ(-10 , a.min());
-}
-
-TEST(Addition, found_min_in_three_el_2) {
-    stack a;
-    a.push(25);
-    a.push(-25);
-    a.push(-10);
-    EXPECT_EQ(-25 , a.min());
-}
-
-TEST(Addition, found_min_in_three_el_3) {
-    stack a;
-    a.push(-15);
-    a.push(150);
-    a.push(-10);
-    EXPECT_EQ(-15 , a.min());
-}
 
 TEST(Addition, test_on_top_and_pop) {
     stack a;
