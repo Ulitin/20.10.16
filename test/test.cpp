8e8e34f96181f0d807b30f0ffd86089aab30c35b
Ulitin <sashavladimirovichulitin@gmail.com>
2016-11-30 23:25:01 +0300

    Merged master fixed conflict 3

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,4 @@
+#include "stack.h"
 #include <gtest/gtest.h>
 
 
@@ -13,6 +14,49 @@ TEST(Addition, test_on_top_and_pop) {
     a.pop();
     EXPECT_FALSE(a.pop());
     ASSERT_ANY_THROW(a.top());
+}
+
+TEST(Addition, correct_with_zero) {
+	stack a;
+	ASSERT_ANY_THROW(a.min());
+}
+
+TEST(Addition, found_min_in_two_el_1) {
+	stack a;
+	a.push(25);
+	a.push(0);
+	EXPECT_EQ(0, a.min());
+}
+
+TEST(Addition, found_min_in_two_el_2) {
+	stack a;
+	a.push(25);
+	a.push(150);
+	EXPECT_EQ(25, a.min());
+}
+
+TEST(Addition, found_min_in_three_el_1) {
+	stack a;
+	a.push(25);
+	a.push(150);
+	a.push(-10);
+	EXPECT_EQ(-10, a.min());
+}
+
+TEST(Addition, found_min_in_three_el_2) {
+	stack a;
+	a.push(25);
+	a.push(-25);
+	a.push(-10);
+	EXPECT_EQ(-25, a.min());
+}
+
+TEST(Addition, found_min_in_three_el_3) {
+	stack a;
+	a.push(-15);
+	a.push(150);
+	a.push(-10);
+	EXPECT_EQ(-15, a.min());
 }
 
 int main(int ac, char* av[]) {
